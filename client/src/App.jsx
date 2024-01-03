6a173ee7aa5c27baf040bf96d0c545a9d232389c
peerawet <peerawet1996@gmail.com>
2024-01-03 11:42:59 +0700

    feat/display user info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { createContext } from "react";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+export const UserDataContext = createContext();
 
 function App() {
   const userData = {
@@ -12,14 +15,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserDataContext.Provider value={userData}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserDataContext.Provider>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import { UserDataContext } from "../App";
+
 function AppHeader() {
+  const userData = useContext(UserDataContext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>
+        สวัสดีคุณ {userData.username} ({userData.level})
+        <img src={userData.avatar}></img>
+      </h2>
     </div>
   );
 }
